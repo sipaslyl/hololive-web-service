9fb3d7568562ee3be3110782a55a9e7d8b089cf9
Lylla <72465355+sipaslyl@users.noreply.github.com>
2021-02-26 13:17:21 -0800

    Adds data sources and reworks plubig to be more topical

---
+++ data/hololiveDebut.js
@@ -0,0 +1,155 @@
+{
+	"title": "Hololive Members by Debut Day",
+	"members":[
+    {
+      "Name": "Tokino Sora",
+      "Generation": 0,
+      "Age": "20",
+      "Gender": "Female",
+      "Birthday": "5/15",
+      "Height": "160 cm",
+      "YoutubeDebutDay": "09/07/2017",
+      "Image": "Tokino_Sora_2020_Portrait.png"
+    },
+    {
+      "Name": "Roboco",
+      "Generation": 0,
+      "Age": "17",
+      "Gender": "Female",
+      "Birthday": "5/23",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "03/04/2018",
+      "Image": "Roboco_Portrait.jpg"
+    },
+    {
+      "Name": "Hoshimachi Suisei",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "5/22",
+      "Height": "160 cm",
+      "YoutubeDebutDay": "03/22/2018",
+      "Image": "Hoshimachi_Suisei_2019_Portrait.png"
+    },
+    {
+      "Name": "Yozora Mei",
+      "Generation": 1,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "10/31",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "05/13/2018",
+      "Image": "Yozora_Mel_Portrait.png"
+    },
+    {
+      "Name": "Shirakami Fubuki",
+      "Generation": 1,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "10/5",
+      "Height": "155 cm",
+      "YoutubeDebutDay": "06/01/2018",
+      "Image": "Shirakami_Fubuki_Portrait.jpg"
+    },
+    {
+      "Name": "Natsuiro Matsuri",
+      "Generation": 1,
+      "Age": "16",
+      "Gender": "Female",
+      "Birthday": "7/22",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "06/01/2018",
+      "Image": "Natsuiro_Matsuri_Portrait.png"
+    },
+    {
+      "Name": "Akai Haato",
+      "Generation": 1,
+      "Age": "19",
+      "Gender": "Female",
+      "Birthday": "8/10",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "06/02/2018",
+      "Image": "Akai_Haato_Portrait.png"
+    },
+    {
+      "Name": "Aki Rosenthal",
+      "Generation": 1,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "2/12",
+      "Height": "162 cm",
+      "YoutubeDebutDay": "06/07/2018",
+      "Image": "Aki_Rosenthal_Portrait.png"
+    },
+    {
+      "Name": "Sakura Miko",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "5/5",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "08/01/2018",
+      "Image": "Sakura_Miko_2019_Portrait.jpg"
+    },
+    {
+      "Name": "Minato Aqua",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/1",
+      "Height": "148 cm",
+      "YoutubeDebutDay": "08/08/2018",
+      "Image": "Minato_Aqua_Portrait.png"
+    },
+    {
+      "Name": "Murasaki Shion",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/8",
+      "Height": "145 cm",
+      "YoutubeDebutDay": "08/17/2018",
+      "Image": "Murasaki_Shion_Portrait.png"
+    },
+    {
+      "Name": "Nakiri Ayame",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/13",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "09/03/2018",
+      "Image": "Nakiri_Ayame_Portrait.png"
+    },
+    {
+      "Name": "Yuzuki Choco",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "2/14",
+      "Height": "165 cm",
+      "YoutubeDebutDay": "09/05/2018",
+      "Image": "Yuzuki_Choco_Portrait.png"
+    },
+    {
+      "Name": "Oozora Subaru",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "7/2",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "09/16/2018",
+      "Image": "Oozora_Subaru_Portrait.png"
+    },
+    {
+      "Name": "AZKi",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "7/1",
+      "Height": "158 cm",
+      "YoutubeDebutDay": "11/15/2018",
+      "Image": "AZKi_2021_Portrait.jpg"
+    }
+   ]
+}
---
+++ data/hololiveGen.js
@@ -0,0 +1,155 @@
+{
+	"title": "Hololive Members by Generation",
+	"members":[
+    {
+      "Name": "Sakura Miko",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "5/5",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "8/1/2018",
+      "Image": "Sakura_Miko_2019_Portrait.jpg"
+    },
+    {
+      "Name": "Roboco",
+      "Generation": 0,
+      "Age": "17",
+      "Gender": "Female",
+      "Birthday": "5/23",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "3/4/2018",
+      "Image": "Roboco_Portrait.jpg"
+    },
+    {
+      "Name": "AZKi",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "7/1",
+      "Height": "158 cm",
+      "YoutubeDebutDay": "11/15/2018",
+      "Image": "AZKi_2021_Portrait.jpg"
+    },
+    {
+      "Name": "Tokino Sora",
+      "Generation": 0,
+      "Age": "20",
+      "Gender": "Female",
+      "Birthday": "5/15",
+      "Height": "160 cm",
+      "YoutubeDebutDay": "9/7/2017",
+      "Image": "Tokino_Sora_2020_Portrait.png"
+    },
+    {
+      "Name": "Hoshimachi Suisei",
+      "Generation": 0,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "5/22",
+      "Height": "160 cm",
+      "YoutubeDebutDay": "3/22/2018",
+      "Image": "Hoshimachi_Suisei_2019_Portrait.png"
+    },
+    {
+      "Name": "Natsuiro Matsuri",
+      "Generation": 1,
+      "Age": "16",
+      "Gender": "Female",
+      "Birthday": "7/22",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "6/1/2018",
+      "Image": "Natsuiro_Matsuri_Portrait.png"
+    },
+    {
+      "Name": "Yozora Mei",
+      "Generation": 1,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "10/31",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "5/13/2018",
+      "Image": "Yozora_Mel_Portrait.png"
+    },
+    {
+      "Name": "Akai Haato",
+      "Generation": 1,
+      "Age": "19",
+      "Gender": "Female",
+      "Birthday": "8/10",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "6/2/2018",
+      "Image": "Akai_Haato_Portrait.png"
+    },
+    {
+      "Name": "Shirakami Fubuki",
+      "Generation": 1,
+      "Age": "18",
+      "Gender": "Female",
+      "Birthday": "10/5",
+      "Height": "155 cm",
+      "YoutubeDebutDay": "6/1/2018",
+      "Image": "Shirakami_Fubuki_Portrait.jpg"
+    },
+    {
+      "Name": "Aki Rosenthal",
+      "Generation": 1,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "2/12",
+      "Height": "162 cm",
+      "YoutubeDebutDay": "6/7/2018",
+      "Image": "Aki_Rosenthal_Portrait.png"
+    },
+    {
+      "Name": "Murasaki Shion",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/8",
+      "Height": "145 cm",
+      "YoutubeDebutDay": "8/17/2018",
+      "Image": "Murasaki_Shion_Portrait.png"
+    },
+    {
+      "Name": "Minato Aqua",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/1",
+      "Height": "148 cm",
+      "YoutubeDebutDay": "8/8/2018",
+      "Image": "Minato_Aqua_Portrait.png"
+    },
+    {
+      "Name": "Nakiri Ayame",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "12/13",
+      "Height": "152 cm",
+      "YoutubeDebutDay": "9/3/2018",
+      "Image": "Nakiri_Ayame_Portrait.png"
+    },
+    {
+      "Name": "Oozora Subaru",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "7/2",
+      "Height": "154 cm",
+      "YoutubeDebutDay": "9/16/2018",
+      "Image": "Oozora_Subaru_Portrait.png"
+    },
+    {
+      "Name": "Yuzuki Choco",
+      "Generation": 2,
+      "Age": "N/A",
+      "Gender": "Female",
+      "Birthday": "2/14",
+      "Height": "165 cm",
+      "YoutubeDebutDay": "9/5/2018",
+      "Image": "Yuzuki_Choco_Portrait.png"
+    }
+   ]
+}
